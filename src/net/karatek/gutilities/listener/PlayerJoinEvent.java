477abf84fd3c7971127bdf7ffda88b3f73000869
KaratekHD <kontakt@karatek.net>
2020-07-04 11:22:51 +0200

    Again some changes for the Developer Mode

--- src/net/karatek/gutilities/listener/PlayerJoinEvent.java
+++ src/net/karatek/gutilities/listener/PlayerJoinEvent.java
@@ -14,6 +14,7 @@ package net.karatek.gutilities.listener;
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import net.karatek.gutilities.main.Main;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,50 +25,42 @@ public class PlayerJoinEvent implements Listener {
     public void onJoin(org.bukkit.event.player.PlayerJoinEvent e) {
         e.setJoinMessage("§7[§a+§7] " + e.getPlayer().getDisplayName());
         Player p = e.getPlayer();
-
-        /*
-        PermissionUser pu = PermissionsEx.getUser(p);
-        if(pu.inGroup("Developer") || pu.inGroup("MainDeveloper")) {
-            boolean devmode = p.hasMetadata("devmode");
-            if(devmode) {
-                p.sendMessage(Main.devprefix + "Entwicklungsmodus: §aaktiviert.");
-                //Send messgeas about the dev modules
-                if(p.hasMetadata("joinmsg")) {
-                    p.sendMessage(Main.devprefix + "Join Debugger: §aaktiviert..");
-                } else {
-                    p.sendMessage(Main.devprefix + "Join Debugger: §cdeaktiviert..");
-                }
-
-                if(p.hasMetadata("scoremsg")) {
-                    p.sendMessage(Main.devprefix + "Scoreboard Debugger: §aaktiviert..");
-                } else {
-                    p.sendMessage(Main.devprefix + "Scoreboard Debugger: §cdeaktiviert..");
-                }
-
-                if(p.hasMetadata("reloadmsg")) {
-                    p.sendMessage(Main.devprefix + "Reload Debugger: §aaktiviert..");
-                } else {
-                    p.sendMessage(Main.devprefix + "Reload Debugger: §cdeaktiviert..");
-                }
-
-                if(p.hasMetadata("cmdspybypass")) {
-                    p.sendMessage(Main.devprefix + "CommandSpy Bypass: §aaktiviert.§r.");
-                }
-
-                if(Main.devlock) {
-                    p.sendMessage(Main.devprefix + "DevLocker: §aeingeschltet§r.");
-                } else {
-                    p.sendMessage(Main.devprefix + "DevLocker: §causgeschltet§r.");
-                }
-
-
-
-
+        boolean devmode = p.hasMetadata("devmode");
+        if (devmode) {
+            p.sendMessage(Main.devprefix + "Entwicklungsmodus: §aaktiviert.");
+            //Send messgeas about the dev modules
+            if (p.hasMetadata("joinmsg")) {
+                p.sendMessage(Main.devprefix + "Join Debugger: §aaktiviert..");
             } else {
-                p.sendMessage(Main.devprefix + "Entwicklungsmodus: §cdeaktiviert.");
+                p.sendMessage(Main.devprefix + "Join Debugger: §cdeaktiviert..");
             }
 
-         */
+            if (p.hasMetadata("scoremsg")) {
+                p.sendMessage(Main.devprefix + "Scoreboard Debugger: §aaktiviert..");
+            } else {
+                p.sendMessage(Main.devprefix + "Scoreboard Debugger: §cdeaktiviert..");
+            }
+
+            if (p.hasMetadata("reloadmsg")) {
+                p.sendMessage(Main.devprefix + "Reload Debugger: §aaktiviert..");
+            } else {
+                p.sendMessage(Main.devprefix + "Reload Debugger: §cdeaktiviert..");
+            }
+
+            if (p.hasMetadata("cmdspybypass")) {
+                p.sendMessage(Main.devprefix + "CommandSpy Bypass: §aaktiviert.§r.");
+            }
+
+            if (Main.devlock) {
+                p.sendMessage(Main.devprefix + "DevLocker: §aeingeschltet§r.");
+            } else {
+                p.sendMessage(Main.devprefix + "DevLocker: §causgeschltet§r.");
+            }
+
+
+        } else {
+            p.sendMessage(Main.devprefix + "Entwicklungsmodus: §cdeaktiviert.");
+        }
         }
 
 
